9eb83be0a62941aeaa2e0a4d409327556b6bb8b3
Davide Barasti <dbarasti@gmail.com>
2020-06-23 18:35:49 +0200

    added support for deactivate command

--- src/deactivate.js
+++ src/deactivate.js
@@ -1,5 +1,37 @@
-function deactivate(argv) {
-  console.log("deactivating campaign " + argv.campaign);
+const Web3 = require('web3');
+const provider = new Web3.providers.HttpProvider("http://localhost:7545");
+const contract = require("@truffle/contract");
+const campaignJSON = require('../../final_project/build/contracts/Campaign.json')
+require("./models/db");
+const checkCampaignRegistration = require("./utils").checkCampaignRegistration;
+
+let CampaignContract = contract(campaignJSON);
+CampaignContract.setProvider(provider);
+
+async function deactivate(argv) {
+  try {
+    await checkCampaignRegistration(argv.campaign);
+  } catch (e) {
+    console.log(e);
+    return;
+  }
+  let instance;
+  try {
+    instance = await CampaignContract.at(argv.campaign);
+  } catch {
+    console.log("Error while getting the contract from the network. Are you sure Ganache is running?");
+    return;
+  }
+  try {
+    await instance.deactivate({
+      from: argv.from
+    });
+  } catch (e) {
+    e.reason == undefined && console.log("Error while deactivating the campaign - " + e)
+    e.reason != undefined && console.log("Error while deactivating the campaign - " + e.reason);
+    return;
+  }
+  console.log("Successful deactivated campaign " + argv.campaign);
 }
 
 module.exports = deactivate;
